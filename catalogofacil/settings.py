03f52256fc2aef90473b7ddf6a42930d9d6fd18f
nicolasmojsiuk <119960899+nicolasmojsiuk@users.noreply.github.com>
2026-03-06 13:37:05 -0300

    Update settings.py

--- catalogofacil/settings.py
+++ catalogofacil/settings.py
@@ -133,9 +133,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'es-ar'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
