3ced8fc869b915245086b3647f8b7a04e6ae7383
nicolasmojsiuk <119960899+nicolasmojsiuk@users.noreply.github.com>
2026-03-06 13:38:00 -0300

    Update admin.py

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Producto, Categoria
-
+from django.contrib.auth.models import User, Group
+from django.contrib import admin
 admin.site.register(Categoria)
 admin.site.register(Producto)
+admin.site.unregister(User)
+admin.site.unregister(Group)
